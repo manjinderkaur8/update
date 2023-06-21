dbfa194adf1d8207abdc451b7aac88b2c2c3bf14
Pranjul Jain <pranjuljainp@gmail.com>
2023-06-21 10:23:02 +0530

    Made changes to career page

--- career/js/style.js
+++ career/js/style.js
@@ -1,27 +1,20 @@
 // Changing Svg position according to window width
 const svg = document.querySelector("svg")
 
-window.addEventListener("resize",function (event){
-    const windowWidth = event.target.innerWidth
-    const difference = 1214 - windowWidth
+// window.addEventListener("resize",function (event){
+//     const windowWidth = event.target.innerWidth
+//     const difference = 1214 - windowWidth
 
-    if(windowWidth > 0){
-        svg.style.top = "calc( -100% + " + (difference/3) + "px )";
-    }
-})
+//     if(difference > 0){
+//         svg.style.top = "calc( -100% + " + (difference) + "px )";
+//     }
+// })
 
 // adding cross icon onclick in navbar
 const navBartoggler = document.querySelector(".navbar-toggler")
 const navIcon = document.querySelector(".navbar-toggler-icon")
 
 navBartoggler.addEventListener('click',function(){
-    // if(navIcon.classList.contains("navbar-toggler-icon")){
-    //     navIcon.s("class","")
-
-    // }
-    // else{
-    //     navIcon.setAttribute("class","navbar-toggler-icon")
-    // }
     navIcon.classList.toggle("navbar-toggler-icon")
     navIcon.classList.toggle("fa-solid")
     navIcon.classList.toggle("fa-xmark")
